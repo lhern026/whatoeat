76be79745c8499cc387ba92a4a4734d64b6d32a0
luis hernandez <lhern026@gmail.com>
2023-04-11 16:58:08 -0700

    push

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -27,8 +27,8 @@ export const authOptions: NextAuthOptions = {
 
 
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
           prompt: "consent",

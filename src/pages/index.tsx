7d9a66a0c87f8e0604f364c3975db33f4014946d
luis hernandez <lhern026@gmail.com>
2023-04-11 17:10:09 -0700

    push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,3 +26,27 @@ const Home = () => {
 };
 
 export default Home;
+
+const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+  //   undefined, // no input
+  //   { enabled: sessionData?.user !== undefined }
+  // );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-white text-center text-2xl">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {/* {secretMessage && <span> - {secretMessage}</span>} */}
+      </p>
+      <button
+        className="bg-white/10 text-white hover:bg-white/20 rounded-full px-10 py-3 font-semibold no-underline transition"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
